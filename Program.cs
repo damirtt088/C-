fb8087f46450530c06e230f4fe69ed5b88aabfd4
damir <dami0407@mail.ru>
2019-05-14 20:20:55 +0300

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,59 +1,143 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace ArrayActions
 {
-    class Point
+    class ArrayActions
     {
-        double _x, _y;
+        private int[] arr;
+        private int sum;
+        private int count;
 
-        public Point()
+        /// <summary>
+        /// конструктор "Задание массива"
+        /// </summary>
+        /// <param name="arrayDimension">количество элементов в массиве</param>
+        /// <param name="OriginalValue">начальное значение</param>
+        /// <param name="StepValue">шаг изменения данных</param>
+        public ArrayActions(int arrayDimension, int OriginalValue, int StepValue)
         {
-            _x = _y = 0;
-        }
-        public Point(double x, double y)
-        {
-            _x = x;
-            _y = y;
-        }
-        public void SetX(double value)
-        {
-            _x = value;
-        }
-        public double GetX()
-        {
-            return _x;
-        }
-        public double X
-        {
-            get { return _x; }
-            set
+            arr = new int[arrayDimension];
+            for (int i = 1; i < arrayDimension; i++)
             {
-                if (value > 0) _x = value;
-                else _x = -value;
+                arr[0] = OriginalValue;
+                arr[i] = arr[i - 1] + StepValue;
             }
         }
-        public double Distance(Point Z)
+        /// <summary>
+        /// Свойство "Доступ к i-ому элементу массива"
+        /// </summary>
+        /// <param name="i">i-ый элемент массива</param>
+        /// <returns></returns>
+        public int this[int i]
         {
-            return Math.Sqrt(Math.Pow(_x - Z._x, 2) + Math.Pow(_y - Z._y, 2));
+            get { return arr[i]; }
+            set { arr[i] = value; }
         }
-        public override string ToString()
+
+        /// <summary>
+        /// метод "Вывод массива на экран"
+        /// </summary>
+        public void ArrayPrint()
         {
-            return _x + "," + _y;
+            Console.Write($"Ваш массив \n");
+            foreach (int i in arr)
+            {
+                Console.WriteLine($"{i} ");
+            }
+            Console.ReadKey();
         }
+
+        
+
     }
 
-    static void Main(string[] args)
+    class Programm
     {
-        Point C = new Point();
-        C.SetX(10);
-        Console.WriteLine(C.GetX());
-        Console.WriteLine(C.X);
-        C.X = 10;
-        Console.WriteLine(C);
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите размерность массива");
+            int arrayDimension = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Введите начальное значение");
+            int OriginalValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Введите шаг изменения значений");
+            int StepValue = Convert.ToInt32(Console.ReadLine());
+            ArrayActions array = new ArrayActions(arrayDimension, OriginalValue, StepValue);      //Создание  экземпляра класса (массива array).
+            array.ArrayPrint();
+            
+            //Console.WriteLine($"Количество максимальных элементов {array.MaxCount}");
+            //Console.ReadKey();
 
-        Point A = new Point();
-        Point B = new Point(2, 2);
-        Console.WriteLine(A.Distance(B));
-        Console.ReadKey();
+
+            Dictionary<int, int> numbers = new Dictionary<int, int>(); //(arrayDimension);
+            {
+                int k = 0;
+                // int i = 0;
+                int j = 1;
+                int dictionaryDemension = 0;
+                numbers.Add(array[0], dictionaryDemension);
+                for (int i = 0; i < arrayDimension; i++)
+                {
+                    if (numbers[array[i], dictionaryDemension] == array[i] && !numbers.ContainsKey(array[i]))
+                    {
+                        j++;
+                        numbers[array[i]] = j;
+                    }
+                    numbers.Add(array[i], dictionaryDemension);
+                }
+
+
+
+
+
+
+
+
+
+
+            }
+            foreach (KeyValuePair<int, int> keyValue in numbers)
+            {
+                Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+            }
+            Console.ReadKey();
+        }
+
+
+
+
+
+        //for (int i = 0; i < arrayDimension; i++)
+        //{
+        //   numbers.Add(arr[0], 1);
+        //    if (array[i] != array[i+1])
+        //    {
+        //        if (!numbers.ContainsKey(intValue))
+        //        {
+        //            numbers.Add(intValue, 0);
+        //        }
+        //        values[numberOfValues] = Convert.ToInt32(intValue);
+        //        numberOfValues++;
+
+        //        numbers[intValue]++;
+        //    }
+        //    else
+        //    {
+        //        //Console.WriteLine("Incorrect value.");
+        //        //incorrectValues++;
+
+        //    }
+        //}
+        //foreach (KeyValuePair<int, int> pair in numbers)
+        //{
+        //    Console.WriteLine("Number {0} occured {1} times.", pair.Key, pair.Value);
+        //}
+
+
+
     }
 }
+
---
+++ Program11.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_3
+{
+    class MyArray
+    {
+        int[] a;
+
+        //  Создание массива и заполнение его случайными числами от min до max
+        /// <summary>
+        /// Метод заполнения массива
+        /// </summary>
+        /// <param name="n">кол-во элементов в массиве</param>
+        /// <param name="min">минимальный предел</param>
+        /// <param name="max">максимальный предел</param>
+        public MyArray(int n, int min, int max)
+        {
+            a = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                a[i] = rnd.Next(min, max);
+        }
+        /// <summary>
+        /// Свойство индексирования для обращения к отдельному элементу массива
+        /// </summary>
+        /// <param name="index">отдельный элемент массива</param>
+        /// <returns>отдельный элемент массива</returns>
+        public int this[int index]
+        {
+            get { return a[index]; }
+            set { a[index] = value; }
+        }
+
+        /// <summary>
+        /// метод "Вывод массива на экран"
+        /// </summary>
+        public void MyArrayPrint()
+        {
+            Console.Write($"Ваш массив \n");
+            foreach (int i in a)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Метод нахождения пар элементов, где один делится нацело на "3"
+        /// </summary>
+        /// <returns>Количество пар</returns>
+        public int Compare()
+        {
+            int theNumberPairsElements = 0;
+            for (int i = 0; i < a.Length - 1; i++)
+                if (a[i] % 3 == 0 ^ a[i + 1] % 3 == 0)
+                    theNumberPairsElements++;
+            return theNumberPairsElements;
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray a = new MyArray(6, -10, 10);
+            a.MyArrayPrint();
+            int t = a.Compare();
+            Console.WriteLine($"\n Количество пар {t}");
+            Console.ReadKey();
+        }
+    }
+
+}
