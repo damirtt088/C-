0cc6bda308b9adfb2e137b0c7635ee5305d1a016
damir <dami0407@mail.ru>
2019-05-17 20:55:20 +0300

    Initial commit

--- Task1.cs
+++ Task1.cs
@@ -3,77 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace Task_1
+namespace Lesson_5
 {
-    class MyArray
-    {
-        int[] a;
-
-        //  Создание массива и заполнение его случайными числами от min до max
-        /// <summary>
-        /// Метод заполнения массива
-        /// </summary>
-        /// <param name="n">кол-во элементов в массиве</param>
-        /// <param name="min">минимальный предел</param>
-        /// <param name="max">максимальный предел</param>
-        public MyArray(int n, int min, int max)
-        {
-            a = new int[n];
-            Random rnd = new Random();
-            for (int i = 0; i < n; i++)
-                a[i] = rnd.Next(min, max);
-        }
-        /// <summary>
-        /// Свойство индексирования для обращения к отдельному элементу массива
-        /// </summary>
-        /// <param name="index">отдельный элемент массива</param>
-        /// <returns>отдельный элемент массива</returns>
-        public int this[int index]
-        {
-            get { return a[index]; }
-            set { a[index] = value; }
-        }
-
-        /// <summary>
-        /// метод "Вывод массива на экран"
-        /// </summary>
-        public void MyArrayPrint()
-        {
-            Console.Write($"Ваш массив \n");
-            foreach (int i in a)
-            {
-                Console.Write($"{i} ");
-            }
-            Console.ReadKey();
-        }
-
-        /// <summary>
-        /// Метод нахождения пар элементов, где один делится нацело на "3"
-        /// </summary>
-        /// <returns>Количество пар</returns>
-        public int Compare()
-        {
-            int theNumberPairsElements = 0;
-            for (int i = 0; i < a.Length - 1; i++)
-                if (a[i] % 3 == 0 ^ a[i + 1] % 3 == 0)
-                    theNumberPairsElements++;
-            return theNumberPairsElements;
-        }
-
-    }
-
-
-    class Program
+    class Task1
     {
         static void Main(string[] args)
         {
-            MyArray a = new MyArray(6, -10, 10);
-            a.MyArrayPrint();
-            int t = a.Compare();
-            Console.WriteLine($"\n Количество пар {t}");
+            string login, pat;
+            bool flag;
+            do
+            {
+                Console.WriteLine("Введите логин (от 2 до 10 символов, первый символ не цифра, только цифры и латинские буквы)");
+                login = Console.ReadLine();
+                pat = @"[a-zA-Z][a-zA-Z0-9]{1,9}";
+                Regex reg = new Regex(pat);
+                flag = reg.IsMatch(pat);
+            }
+            while (flag == false);
+            Console.WriteLine($"Пароль соответствует требованиям - {login}");
             Console.ReadKey();
+
+            //if (reg.IsMatch(login))
+            //{
+            //    Console.WriteLine($"Пароль соответствует требованиям - {login}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Пароль не соответствует требованиям - {login}");
+            //}
+            // Console.ReadKey();
         }
     }
-
 }
