0d6799cfaaace3addb024d2028a859698d683f0d
damir <dami0407@mail.ru>
2019-05-21 21:44:08 +0300

    Initial commit

--- Task1.cs
+++ Task1.cs
@@ -3,37 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
-namespace Lesson_5
+namespace Task1
 {
+    // Описываем делегат. 
+    public delegate double Fun(double y, double a);
+       
     class Task1
     {
+        // Создаем метод, который принимает делегат
+        // На практике этот метод сможет принимать любой метод
+        // с такой же сигнатурой, как у делегата
+        public static void Table(Fun F, double x, double b, double a)
+        {
+            Console.WriteLine("----- X ----- Y -----");
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x, a));
+                x += 1;
+            }
+            Console.WriteLine("---------------------");
+        }
+
+        /// <summary>
+        /// метод MyFuncXCvadrat(a* sin(x)) для передачи его в качестве параметра в Table
+        /// </summary>
+        /// <param name="x">переменная для передачи в sin()</param>
+        /// <param name="a">консольно задаваемый параметр для умножения на sin()</param>
+        /// <returns>возвращает значение выражения a * Math.Sin(x)</returns>
+        public static double MyFuncXCvadrat(double y, double a)
+        {
+            return a * Math.Sin(y);
+        }
+        
+        /// <summary>
+        /// метод MyFuncXSINUS (a*x*x) для передачи его в качестве параметра в Table
+        /// </summary>
+        /// <param name="x">параметр для возведения в квадрат</param>
+        /// <param name="a">консольно задаваемый параметр для умножения на квадрат "х"</param>
+        /// <returns>возвращает значение выражения a * x * x </returns>
+        public static double MyFuncXSINUS(double y, double a)
+        {
+            return a * y * y;
+        }
+                     
+
         static void Main(string[] args)
         {
-            string login, pat;
-            bool flag;
-            do
-            {
-                Console.WriteLine("Введите логин (от 2 до 10 символов, первый символ не цифра, только цифры и латинские буквы)");
-                login = Console.ReadLine();
-                pat = @"[a-zA-Z][a-zA-Z0-9]{1,9}";
-                Regex reg = new Regex(pat);
-                flag = reg.IsMatch(pat);
-            }
-            while (flag == false);
-            Console.WriteLine($"Пароль соответствует требованиям - {login}");
+            Console.WriteLine("Введите параметр а ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите параметр x ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Fun F = MyFuncXCvadrat;
+            Table(F, -2, 2, a);
+            F = MyFuncXCvadrat;
+            Table(F, -2, 2, a);
             Console.ReadKey();
-
-            //if (reg.IsMatch(login))
-            //{
-            //    Console.WriteLine($"Пароль соответствует требованиям - {login}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine($"Пароль не соответствует требованиям - {login}");
-            //}
-            // Console.ReadKey();
+                                                                             
+            
         }
     }
 }
+
+
